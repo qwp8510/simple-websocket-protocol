8bb64638d484cc60eb2d52f4bc2b9cd235eaf440
weichen <a4793706@gmail.com>
2022-08-27 10:08:40 +0800

    upload script.js

---
+++ script.js
@@ -0,0 +1,83 @@
+var $messages = $('.messages-content'),
+    d, h, m,
+    i = 0;
+
+WS_URL = 'ws://localhost:6633'
+var socket = new WebSocket(WS_URL);
+
+socket.addEventListener('open', function (event) {
+    socket.send('websocket connected');
+});
+
+// Listen for messages
+socket.addEventListener('message', function (event) {
+    NewMessage(event.data)
+    console.log('Message from server ', event);
+});
+
+function wsSend(msg) {
+    socket.send(msg);
+}
+
+$(window).load(function() {
+  $messages.mCustomScrollbar();
+  // setTimeout(function() {
+  //   NewMessage();
+  // }, 100);
+});
+
+function updateScrollbar() {
+  $messages.mCustomScrollbar("update").mCustomScrollbar('scrollTo', 'bottom', {
+    scrollInertia: 10,
+    timeout: 0
+  });
+}
+
+function setDate(){
+  d = new Date()
+  if (m != d.getMinutes()) {
+    m = d.getMinutes();
+    $('<div class="timestamp">' + d.getHours() + ':' + m + '</div>').appendTo($('.message:last'));
+  }
+}
+
+function insertMessage() {
+  msg = $('.message-input').val();
+  if ($.trim(msg) == '') {
+    return false;
+  }
+  wsSend(msg);
+  $('<div class="message message-personal">' + msg + '</div>').appendTo($('.mCSB_container')).addClass('new');
+  setDate();
+  $('.message-input').val(null);
+  updateScrollbar();
+}
+
+$('.message-submit').click(function() {
+  insertMessage();
+});
+
+$(window).on('keydown', function(e) {
+  if (e.which == 13) {
+    insertMessage();
+    return false;
+  }
+})
+
+
+function NewMessage(msg) {
+  if ($('.message-input').val() != '') {
+    return false;
+  }
+  $('<div class="message loading new"><figure class="avatar"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/156381/profile/profile-80.jpg" /></figure><span></span></div>').appendTo($('.mCSB_container'));
+  updateScrollbar();
+
+  setTimeout(function() {
+    $('.message.loading').remove();
+    $('<div class="message new"><figure class="avatar"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/156381/profile/profile-80.jpg" /></figure>' + msg + '</div>').appendTo($('.mCSB_container')).addClass('new');
+    setDate();
+    updateScrollbar();
+    i++;
+  }, 1000 + (Math.random() * 20) * 100);
+
+}
